54608466f40ecc058a39ca4d58ae6dd2d62a8370
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-10 20:45:56 +0300

    Query BETWEEN

--- queries.sql
+++ queries.sql
@@ -1,3 +1,15 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT
+    *
+from
+    animals
+WHERE
+    name = 'Luna';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT
+    name
+FROM
+    animals
+WHERE
+    date_of_birth BETWEEN '2016-01-01'
+    AND '2019-12-31';
