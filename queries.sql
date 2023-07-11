84798daf90360d39055269c9d1a06eda55ca6dad
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-11 12:06:44 +0300

    Update and Commit

--- queries.sql
+++ queries.sql
@@ -114,5 +114,18 @@ WHERE
     date_of_birth > 'Jan 1, 2022';
 
 SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg * -1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1;
+
 ROLLBACK TO SP1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1
+WHERE
+    weight_kg < 0;
+COMMIT;
